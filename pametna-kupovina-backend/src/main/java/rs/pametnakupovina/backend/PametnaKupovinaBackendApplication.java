a039fdeaa8a56cccc3a4b118eea233d9166f6da3
Milki19 <mstefan558@gmail.com>
2026-07-21 13:10:28 +0200

    Initialized backend and local PostGIS infrastructure

---
+++ pametna-kupovina-backend/src/main/java/rs/pametnakupovina/backend/PametnaKupovinaBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.pametnakupovina.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PametnaKupovinaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PametnaKupovinaBackendApplication.class, args);
+    }
+
+}
---
+++ pametna-kupovina-backend/src/test/java/rs/pametnakupovina/backend/PametnaKupovinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.pametnakupovina.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PametnaKupovinaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
